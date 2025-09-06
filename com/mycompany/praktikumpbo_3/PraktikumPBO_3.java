bfa5e3f63307dc356d504243903cfc61a05c176c
Zahra Nibras Hanilatifa <zahrahani003@gmail.com>
2025-09-06 10:49:05 +0700

    Add Project NetBeans

---
+++ com/mycompany/praktikumpbo_3/PraktikumPBO_3.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.praktikumpbo_3;
+
+/**
+ *
+ * @author Comp HP
+ */
+public class PraktikumPBO_3 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
